312e5e01d1352178af4feefe4764feaad71320aa
Mario Juarez <juarez.mario.r@gmail.com>
2025-02-18 15:47:32 -0300

    envio de whatsapps

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "https://graph.facebook.com/v22.0/569259382937171/messages"; // Endpoint de Meta API
+        string token = "EAASYXrmdB08BO1auQOFJQzqAq3zySezgHUG4Q2KNrcvLlYM9xZCqtRSg9b1DZBSfBUZBCrh12zR6IJwSSJZCCSvgjhirq7J3HPosQzXGN5OGTs0llgzZAf0TxAO8I9zTfpck1xcDudtvVx1dlC4bq5fsZCkyxBcRBMa0mJz0R6VgWo5slUm2egLpq6gbS92MclKoBTcxO0TX6TWsLURfMEqKZC0ZCGER5jvEBngu89dD";
+        string toNumber = "+541132542221"; // Número de destino con código de país
+        string jsonData = "{ \"messaging_product\": \"whatsapp\", \"to\": \"" + toNumber + "\", \"type\": \"template\", \"template\": { \"name\": \"hello_world\", \"language\": { \"code\": \"en_US\" } } }";
+
+        try
+        {
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            // Crear la solicitud HTTP POST
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Headers["Authorization"] = "Bearer " + token;
+
+            // Convertir el JSON a bytes
+            byte[] byteArray = Encoding.UTF8.GetBytes(jsonData);
+            request.ContentLength = byteArray.Length;
+
+            // Escribir el JSON en la solicitud
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            // Obtener la respuesta
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                string responseText = reader.ReadToEnd();
+                Console.WriteLine("Respuesta de WhatsApp API: " + responseText);
+            }
+        }
+        catch (WebException ex)
+        {
+            using (StreamReader reader = new StreamReader(ex.Response.GetResponseStream()))
+            {
+                string errorResponse = reader.ReadToEnd();
+                Console.WriteLine("Error: " + errorResponse);
+            }
+        }
+    }
+}
--- web_app_mensajes/Controllers/MensajeTempsController.cs
+++ web_app_mensajes/Controllers/MensajeTempsController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using web_app_mensajes;
@@ -17,6 +19,7 @@ namespace web_app_mensajes.Controllers
         // GET: MensajeTemps
         public ActionResult Index()
         {
+            //Enviar_Whatsapps();
             return View(db.MensajeTemp.ToList());
         }
 
@@ -66,6 +69,11 @@ namespace web_app_mensajes.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             MensajeTemp mensajeTemp = db.MensajeTemp.Find(id);
+
+            var telefono = mensajeTemp.telefono;
+
+            Enviar_Whatsapps(telefono);
+
             if (mensajeTemp == null)
             {
                 return HttpNotFound();
@@ -82,6 +90,7 @@ namespace web_app_mensajes.Controllers
         {
             if (ModelState.IsValid)
             {
+               
                 db.Entry(mensajeTemp).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -122,6 +131,50 @@ namespace web_app_mensajes.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+        static void Enviar_Whatsapps(string tel)
+        {
+            string url = "https://graph.facebook.com/v22.0/569259382937171/messages"; // Endpoint de Meta API
+            string token = "EAASYXrmdB08BO1auQOFJQzqAq3zySezgHUG4Q2KNrcvLlYM9xZCqtRSg9b1DZBSfBUZBCrh12zR6IJwSSJZCCSvgjhirq7J3HPosQzXGN5OGTs0llgzZAf0TxAO8I9zTfpck1xcDudtvVx1dlC4bq5fsZCkyxBcRBMa0mJz0R6VgWo5slUm2egLpq6gbS92MclKoBTcxO0TX6TWsLURfMEqKZC0ZCGER5jvEBngu89dD";
+            string toNumber = "+541132542221"; // Número de destino con código de país
+            string jsonData = "{ \"messaging_product\": \"whatsapp\", \"to\": \"" + "+" + tel + "\", \"type\": \"template\", \"template\": { \"name\": \"hello_world\", \"language\": { \"code\": \"en_US\" } } }";
+
+            try
+            {
+
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                // Crear la solicitud HTTP POST
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.Headers["Authorization"] = "Bearer " + token;
+
+                // Convertir el JSON a bytes
+                byte[] byteArray = Encoding.UTF8.GetBytes(jsonData);
+                request.ContentLength = byteArray.Length;
+
+                // Escribir el JSON en la solicitud
+                using (Stream dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
+
+                // Obtener la respuesta
+                using (WebResponse response = request.GetResponse())
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string responseText = reader.ReadToEnd();
+                    Console.WriteLine("Respuesta de WhatsApp API: " + responseText);
+                }
+            }
+            catch (WebException ex)
+            {
+                using (StreamReader reader = new StreamReader(ex.Response.GetResponseStream()))
+                {
+                    string errorResponse = reader.ReadToEnd();
+                    Console.WriteLine("Error: " + errorResponse);
+                }
+            }
         }
     }
 }
